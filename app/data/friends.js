05a60a29873116a9d960a29bfb0c91d20c50d24b
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-05-01 21:46:36 -0400

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendsList = [
+{"name": "Adit",
+"photo":"https://sites.google.com/a/mail.usf.edu/sustainabilitynews/_/rsrc/1468857631792/articles/aditpatelworkingforthesarasotacountygovernment/AAEAAQAAAAAAAAYwAAAAJGFkNzkxNzkyLTQ2YzctNGQ0My05NmNjLTVlMDM0ZWI0NzY3Ng.jpg",
+"scores":["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+},
+
+{"name": "Lebron",
+"photo":"https://mk0slamonlinensgt39k.kinstacdn.com/wp-content/uploads/2018/04/GettyImages-942823880.jpg",
+"scores":["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+},
+{"name": "Kobe",
+"photo":"https://cdn-s3.si.com/s3fs-public/2017/12/18/kobe_bryant_24.jpg",
+"scores":["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+}
+]
+
+module.exports = friendsList;
